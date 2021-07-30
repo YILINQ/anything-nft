5f097de0b757fcb8a37cd772201473c9383341da
Lizhi Yu <leedsyu@yahoo.com>
2021-07-30 14:30:23 -0400

    implemented purchase-album

--- backend/src/controllers/collectionController.js
+++ backend/src/controllers/collectionController.js
@@ -101,6 +101,9 @@ async function purchaseNtf(req, res) {
     nft.owner = [{ "address": transaction.buyer, "percentage": 1 }];
     nft.status = "private";
 
+    //TODO: check if this nft fullfilled a album
+
+
     await saveAll([nft, transaction, buyer, seller])
         .then(() => {
             return res.status(200).send();
@@ -125,32 +128,37 @@ async function purchaseAlbum(req, res) {
     if (album.status !== "for sale") {
         return res.status(400).json({ error: "album is not for sale" });
     }
-
     if (album.owner === body.buyer) {
         return res.status(400).json({ error: "buyer is owner" });
     }
 
+    // check if any associated nft is funded or if any associated nft is not owned by album owner
     const nft_ids = album.nft_ids;
+    const nfts = [];
     const nft_owners = [];
-    nft_ids.forEach((nft_d) => {
+    await nft_ids.forEach(async (nft_d) => {
         const nft = await Nft.findOne({ nft_id: body.nft_id });
         const curr_nft_owners = nft.owner.map((element) => { return element.address; });
+        nfts.push(nft);
         nft_owners = [...nft_owners, ...curr_nft_owners]
     })
-    if (nft_owners.length !== nft_ids.length or )
+    if (nft_owners.length !== nft_ids.length || Set(nft_owners).size !== 1) {
+        return res.status(400).json({ error: "album is not owned by a single owner" });
+    }
 
     // create a transaction record
     const transaction = new Transaction({
         buyer: body.buyer,
-        seller: nft.owner[0].address,
-        transaction_type: 'purchase-nft',
-        price: nft.price,
-        currency: nft.currency,
+        seller: album.owner,
+        transaction_type: 'purchase-album',
+        price: album.price,
+        currency: album.currency,
         commission: body.commission,
         commission_currency: body.commission_currency,
-        collection_id: nft.nft_id
+        collection_id: album.album_id
     });
 
+    // get buyer/seller 
     let buyer = await User.findOne({ address: transaction.buyer });
     let seller = await User.findOne({ address: transaction.seller });
 
@@ -158,18 +166,24 @@ async function purchaseAlbum(req, res) {
         return res.status(404).json({ error: "user not found" });
     }
 
-    // add/remove nft from buyer/seller's prfoile
-    buyer.nft_ids.push(transaction.collection_id);
-    const index = seller.nft_ids.indexOf(transaction.collection_id);
+    // add/remove album from buyer/seller's prfoile
+    buyer.album_ids.push(transaction.collection_id);
+    const index = seller.album_ids.indexOf(transaction.collection_id);
     if (index !== -1) {
-        seller.nft_ids.splice(index, 1);
+        seller.album_ids.splice(index, 1);
     }
 
-    // update nft's information
-    nft.owner = [{ "address": transaction.buyer, "percentage": 1 }];
-    nft.status = "private";
+    // update album's information
+    album.owner = transaction.buyer
+    album.status = "private";
 
-    await saveAll([nft, transaction, buyer, seller])
+    //update associated nft's information
+    nfts.forEach((nft) => {
+        nft.owner = [{ "address": transaction.buyer, "percentage": 1 }];
+        nft.status = "private";
+    })
+
+    await saveAll([...nfts, transaction, buyer, seller])
         .then(() => {
             return res.status(200).send();
         })
@@ -177,7 +191,6 @@ async function purchaseAlbum(req, res) {
             return res.status(422).json({ error: error.message });
         });
 };
-
 
 
 module.exports = {
